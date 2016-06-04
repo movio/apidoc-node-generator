c992d49673aa120eb8daec5330f155e6181a2f16
kalmanb <kalman@movio.co>
2016-06-04 22:20:44 +1200

    Add first full code test
    Still needs to be implemented

--- server/generators.js
+++ server/generators.js
@@ -1,7 +1,7 @@
 import * as _ from 'lodash/fp';
 
 import jsonSchema from './jsonSchema';
-import reduxSaga from './reduxSaga';
+import * as reduxSaga from './reduxSaga';
 
 const generators = {
   jsonSchema,
--- test/reduxSaga/reduxSaga.spec.js
+++ test/reduxSaga/reduxSaga.spec.js
@@ -4,10 +4,97 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import app from '../../server/index';
-import reduxSaga from '../../server/reduxSaga';
-const createName = reduxSaga.createName;
+import { createName, createContents } from '../../server/reduxSaga';
 
 describe('Redux Saga', () => {
+    const contents = `// This file is generated
+
+import { takeEvery, takeLatest } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+
+function api(aPathParam, aQueryParam) {
+  return new Promise((resolve) => {
+    // Use superagent
+    const response = {
+      todos: [
+        {
+          text: 'Use Redux',
+          completed: false,
+        },
+        {
+          text: 'Use Saga',
+          completed: false,
+        },
+      ],
+    };
+    resolve(response);
+  });
+}
+
+const actionTypes = {
+  getTodos_get: 'getTodos/get',
+  getTodos_doing: 'getTodos/doing',
+  getTodos_success: 'getTodos/success',
+  getTodos_failure: 'getTodos/failure',
+};
+
+const actions = {
+  getTodos_get: (aPathParam, aQueryParam) => ({
+    type: actionTypes.getTodos_get,
+    payload: {
+      aPathParam,
+      aQueryParam,
+    },
+  }),
+  getTodos_doing: () => ({
+    type: actionTypes.getTodos_doing,
+  }),
+  getTodos_success: (todos) => ({
+    type: actionTypes.getTodos_success,
+    payload: todos,
+  }),
+  getTodos_failure: (err) => ({
+    type: actionTypes.getTodos_failure,
+    payload: err,
+    error: true,
+  }),
+};
+
+function* saga(action) {
+  const { aPathParam, aQueryParam } = action.payload;
+  try {
+    yield put(actions.getTodos_doing());
+    const { response } = yield call(api, aPathParam, aQueryParam);
+    yield put(actions.getTodos_success(response));
+  } catch (error) {
+    yield put(actions.getTodos_failure(error));
+  }
+}
+
+/**
+ * Start this saga if you'd prefer to process every action
+ */
+function* takeEverySaga() {
+  yield* takeEvery(actionTypes.getTodos_get, saga);
+}
+
+/**
+ * Start this saga if you'd prefer to process only the latest action
+ */
+function* takeLatestSaga() {
+  yield* takeLatest(actionTypes.getTodos_get, saga);
+}
+
+export {
+  actions,
+  actionTypes,
+  api,
+  saga,
+  takeEverySaga,
+  takeLatestSaga,
+};
+`
+
   it('first', (done) => {
     const service = {
       service: {
@@ -102,17 +189,18 @@ describe('Redux Saga', () => {
       },
     };
 
+
     const expected = {
       files: [
         {
           name: 'getGenerators',
           dir: 'generator',
-          contents: 'hey',
+          contents: contents,
         },
         {
           name: 'getGeneratorsByKey',
           dir: 'generator',
-          contents: 'hey',
+          contents: contents,
         },
       ],
     };
@@ -124,7 +212,7 @@ describe('Redux Saga', () => {
       .expect(expected, done);
   });
 
-  it('createName', (done) => {
+  it('createName', () => {
     expect(createName('GET', '/generators')).to.equal('getGenerators');
     expect(createName('POST', '/generators')).to.equal('postGenerators');
     expect(createName('PUT', '/generators')).to.equal('putGenerators');
@@ -132,6 +220,11 @@ describe('Redux Saga', () => {
     expect(createName('GET', '/generators/person/:id/:date'))
       .to.equal('getGeneratorsAndPersonByIdAndDate');
     expect(createName('GET', '/_generators_/person/:id')).to.equal('getGeneratorsAndPersonById');
-    done();
+  });
+
+  it('should return generated code', () => {
+    const resource = '';
+    const operation = '';
+    expect(createContents(resource, operation)).to.equal(contents);
   });
 });
