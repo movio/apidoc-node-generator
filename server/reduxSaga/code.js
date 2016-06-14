cea406e433f6b90a0a9e5f1042b06a8d73bf2330
kalmanb <kalman@movio.co>
2016-06-14 16:52:54 +1200

    Add full support for params, body and post

--- server/reduxSaga/code.js
+++ server/reduxSaga/code.js
@@ -1,15 +1,51 @@
-import HandleBars from 'handlebars';
+import * as _ from 'lodash/fp';
+import Handlebars from 'handlebars';
 import fs from 'fs';
 
 import { createName } from './index';
 
 const templateSrc = fs.readFileSync(__dirname + '/mainTemplate.handlebars', 'utf8');
-const template = HandleBars.compile(templateSrc);
+const template = Handlebars.compile(templateSrc);
 
-export default function code(operation) {
+function parameterList(params, prefix = '', lines = false) {
+  const vals = _.map((param) => param.name, params)
+  const separator = lines ? ',\n      ' : ', ';
+  return prefix + vals.join(separator);
+}
+
+Handlebars.registerHelper('parameterList', (operation, options) => {
+  const body = operation.body ? [{name:'__body'}] : [];
+  const prefix = operation.parameters.length > 0 && options.hash.prefix ? options.hash.prefix : '';
+  const lines = options.hash.lines ? true : false;
+  return parameterList(body.concat(operation.parameters), prefix, lines);
+});
+
+Handlebars.registerHelper('toLowerCase', (str) => {
+  return str.toLowerCase();
+});
+
+Handlebars.registerHelper('jsdocParameter', (parameter) => {
+  const print = (str) => str ? str : '';
+  const optionalEquals = parameter.required ? '' : '=';
+  const optionalDescription = parameter.required ? '' : ' (Optional)';
+
+  return `* @param {${parameter.type}${optionalEquals}} ${parameter.name} - ${print(parameter.description)}${optionalDescription}`
+});
+
+function removeFormParams(parameters) {
+  return _.filter((p) => p.location.toLowerCase() !== 'form', parameters);
+}
+
+export default function code(rawOperation) {
+  const parameters = removeFormParams(rawOperation.parameters);
+  const operation = {
+    ...rawOperation,
+    parameters
+  };
+
   const context = {
     name: createName(operation.method, operation.path),
-    method: operation.method.toLowerCase(),
-  }
+    operation,
+  };
   return template(context);
 }
--- test/reduxSaga/code.spec.js
+++ test/reduxSaga/code.spec.js
@@ -4,17 +4,80 @@ import fs from 'fs';
 
 import { code } from '../../server/reduxSaga';
 
-import service from '../reduxSaga/generatorService.json';
-
 describe('Redux Saga Code Gen', () => {
-  const getGeneratorExpected = fs.readFileSync(__dirname + '/getGeneratedExpected.txt', 'utf8');
+  const getTodos = fs.readFileSync(__dirname + '/getTodosExpected.txt', 'utf8');
+  const getTodosByKey1AndKey2 = fs.readFileSync(__dirname + '/getTodosByKey1AndKey2Expected.txt', 'utf8');
+  const postTodosByKey = fs.readFileSync(__dirname + '/postTodosByKeyExpected.txt', 'utf8');
 
-  it('should return generated code', () => {
+  it('should create code for GET with no params', () => {
     const operation = {
       method: 'GET',
       path: 'todos',
       parameters: [],
     }
-    expect(code(operation)).to.equal(getGeneratorExpected);
+    expect(code(operation)).to.equal(getTodos);
+  });
+
+  it('should create code for GET with keys', () => {
+    const operation = {
+      method: 'GET',
+      path: 'todos/:key1/:key2',
+      description: 'All the things',
+      parameters: [
+        {
+          name: 'key1',
+          location: 'Path',
+          type: 'string',
+          required: true,
+          description: 'the first key',
+        },
+        {
+          name: 'key2',
+          location: 'Path',
+          type: 'integer',
+          required: true,
+          description: 'the second key',
+        },
+        {
+          name: 'firstName',
+          location: 'Query',
+          type: 'string',
+          required: true,
+          description: 'first comes before last',
+        },
+        {
+          name: 'lastName',
+          location: 'Query',
+          type: 'string',
+          required: false,
+          description: 'last name'
+        },
+      ],
+    }
+    expect(code(operation)).to.equal(getTodosByKey1AndKey2);
+  });
+
+  it('should create code for POST', () => {
+    const operation = {
+      method: 'POST',
+      path: 'todos',
+      body: {type: 'string'},
+      parameters: [
+        {
+          name: 'key',
+          location: 'Query',
+          type: 'string',
+          required: true,
+        },
+        {
+          name: 'shouldBeIgnored',
+          location: 'Form',
+          type: 'string',
+          required: false,
+        },
+      ],
+    }
+    //fixme - add params
+    expect(code(operation)).to.equal(postTodosByKey);
   });
 });
