4507180837c6271464865761e7e9bbca6c145a3f
kalmanb <kalman@movio.co>
2016-06-23 11:16:42 +1200

    Add eslint rules

--- server/reduxSaga/code.js
+++ server/reduxSaga/code.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 import { createName } from './index';
 
-const templateSrc = fs.readFileSync(__dirname + '/mainTemplate.handlebars', 'utf8');
+const templateSrc = fs.readFileSync(`${__dirname}/mainTemplate.handlebars`, 'utf8');
 const template = Handlebars.compile(templateSrc);
 
 function parameterList(params, lines = false) {
@@ -14,14 +14,11 @@ function parameterList(params, lines = false) {
 }
 
 Handlebars.registerHelper('parameterList', (operation, options) => {
-  const body = operation.body ? [{name:'__body'}] : [];
-  const lines = options.hash.lines ? true : false;
-  return parameterList(body.concat(operation.parameters), lines);
+  const body = operation.body ? [{ name: '__body' }] : [];
+  return parameterList(body.concat(operation.parameters), options.hash.lines);
 });
 
-Handlebars.registerHelper('toLowerCase', (str) => {
-  return str.toLowerCase();
-});
+Handlebars.registerHelper('toLowerCase', (str) => str.toLowerCase());
 
 Handlebars.registerHelper('jsdocParameter', (parameter) => {
   const print = (str) => str ? str : '';
@@ -39,7 +36,7 @@ export default function code(rawOperation) {
   const parameters = removeFormParams(rawOperation.parameters);
   const operation = {
     ...rawOperation,
-    parameters
+    parameters,
   };
 
   const context = {
