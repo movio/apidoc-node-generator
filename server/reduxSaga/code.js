aab2436e9884e2ef3099fb35493797f5bfd2537c
jackhopner <jack@movio.co>
2016-07-07 16:13:39 +1200

    Formatting, defaults and request

--- server/reduxSaga/code.js
+++ server/reduxSaga/code.js
@@ -7,15 +7,36 @@ import { createName } from './index';
 const templateSrc = fs.readFileSync(`${__dirname}/mainTemplate.handlebars`, 'utf8');
 const template = Handlebars.compile(templateSrc);
 
-function parameterList(params, lines = false) {
-  const vals = _.map((param) => param.name, params);
-  const separator = lines ? ',\n      ' : ', ';
-  return vals.join(separator);
+function parameterList(params, lines = false, initUndefined = false, indent=1) {
+  return params.map((param) => createParameter(param, initUndefined)).join(`,${lines ? '\n' : ''}${' '.repeat(indent)}`);
+}
+
+function createParameter(param, initUndefined) {
+  return `${param.name}${initUndefined && ! param.required ? ' = undefined' : ''}`;
+}
+
+function createObjectList(params, indent=2) {
+  return params.map((param) => `${param.name} ? { ${param.name} } : {}`).join(`,\n${' '.repeat(indent)}`);
+}
+
+function createPath(operation) {
+  const rex = /:([^\/]+)/gi;
+  return operation.path.replace(rex, '\${$1}');
 }
 
 Handlebars.registerHelper('parameterList', (operation, options) => {
   const body = operation.body ? [{ name: '__body' }] : [];
-  return parameterList(body.concat(operation.parameters), options.hash.lines);
+  return parameterList(body.concat(operation.parameters), options.hash.lines, options.hash.initUndefined, options.hash.indent);
+});
+
+Handlebars.registerHelper('createObjectList', (operation, options) => {
+  const body = operation.body ? [{ name: '__body' }] : [];
+  return createObjectList(body.concat(operation.parameters.filter((param) => param.location === 'Query')), options.hash.indent);
+});
+
+Handlebars.registerHelper('createPath', (operation) => {
+  const body = operation.body ? [{ name: '__body' }] : [];
+  return createPath(operation);
 });
 
 Handlebars.registerHelper('toLowerCase', (str) => str.toLowerCase());
